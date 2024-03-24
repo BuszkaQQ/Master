8e1de41de3dd55b9cb7c437bdc9702c0e00bb123
BuszkaQQ <164628931+BuszkaQQ@users.noreply.github.com>
2024-03-24 11:35:36 +0100

    Add files via upload

---
+++ 1_zadanie_Tomasz_Buszka.py
@@ -0,0 +1,20 @@
+kilometry = input()
+
+milimetry = int(kilometry) * 1000000
+
+centymetry = int(kilometry) * 100000
+
+metry = int(kilometry) * 1000
+
+cale = int(kilometry) * 39370.
+
+stopy = int(kilometry) * 3280.8
+
+mile = int(kilometry) * 0.62137
+
+print (f" Kilometry przekonwertowane na milimetry to {milimetry}")
+print (f" Kilometry przekonwertowane na centymetry to {centymetry}")
+print (f" Kilometry przekonwertowane na metry to {metry}")
+print (f" Kilometry przekonwertowane na cale to {cale}")
+print (f" Kilometry przekonwertowane na stopy to {stopy}")
+print (f" Kilometry przekonwertowane na mile to {mile}")
